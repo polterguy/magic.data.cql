6d60538c54b05b8017a17f041eb48b0590e0af0c
Thomas Hansen <thomas@gaiasoul.com>
2022-01-19 11:42:44 +0200

    Creating slots

---
+++ magic.data.cql/slots/Connect.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using magic.node;
+using magic.data.cql.io;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.data.cql.slots
+{
+    /// <summary>
+    /// [cql.connect] slot, for connecting to a CQL database instance,
+    /// according to your configuration settings.
+    /// </summary>
+    [Slot(Name = "cql.connect")]
+    public class Connect : ISlot, ISlotAsync
+    {
+        readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Creates a new instance of your type.
+        /// </summary>
+        /// <param name="configuration">Configuration for your application.</param>
+        public Connect(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Implementation of your slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to raise the signal.</param>
+        /// <param name="input">Arguments to your slot.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            using (var session = CqlFileService.CreateSession(_configuration, input.GetEx<string>()))
+            {
+                signaler.Scope(
+                    "cql.connect",
+                    session,
+                    () => signaler.Signal("eval", input));
+                input.Value = null;
+            }
+        }
+
+        /// <summary>
+        /// Implementation of your slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to raise the signal.</param>
+        /// <param name="input">Arguments to your slot.</param>
+        /// <returns>An awaitable task.</returns>
+        public async Task SignalAsync(ISignaler signaler, Node input)
+        {
+            using (var session = CqlFileService.CreateSession(_configuration, input.GetEx<string>()))
+            {
+                await signaler.ScopeAsync(
+                    "cql.connect",
+                    session,
+                    async () => await signaler.SignalAsync("eval", input));
+                input.Value = null;
+            }
+        }
+    }
+}
---
+++ magic.data.cql/slots/Execute.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+using System.Linq;
+using System.Threading.Tasks;
+using Cassandra;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.data.cql.slots
+{
+    /// <summary>
+    /// [cql.execute] slot, for executing som CQL towards a keyspace and returning the result of the invocation to caller.
+    /// </summary>
+    [Slot(Name = "cql.execute")]
+    public class Execute : ISlot, ISlotAsync
+    {
+        /// <summary>
+        /// Implementation of your slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to raise the signal.</param>
+        /// <param name="input">Arguments to your slot.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            SignalAsync(signaler, input).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Implementation of your slot.
+        /// </summary>
+        /// <param name="signaler">Signaler used to raise the signal.</param>
+        /// <param name="input">Arguments to your slot.</param>
+        /// <returns>An awaitable task.</returns>
+        public async Task SignalAsync(ISignaler signaler, Node input)
+        {
+            var cql = input.GetEx<string>();
+            var args = input.Children.ToDictionary(x => x.Name, x => x.Value);
+            input.Clear();
+            input.Value = null;
+            var rowSet = await signaler.Peek<ISession>("cql.connect").ExecuteAsync(new SimpleStatement(args, cql));
+            foreach (var idx in rowSet)
+            {
+                var cur = new Node(".");
+                for (var idxNo = 0; idxNo < idx.Length; idxNo++)
+                {
+                    cur.Add(new Node(rowSet.Columns[idxNo].Name, idx[idxNo]));
+                }
+                input.Add(cur);
+            }
+        }
+    }
+}
