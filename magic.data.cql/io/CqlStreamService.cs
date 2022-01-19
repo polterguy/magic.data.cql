ecf323a88c038c778b2233142e9bdf908dccbb17
Thomas Hansen <thomas@gaiasoul.com>
2022-01-19 22:19:48 +0200

    Creating stream service
    By simply forwarding to IFileService

---
+++ magic.data.cql/io/CqlStreamService.cs
@@ -0,0 +1,57 @@
+/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using magic.node.contracts;
+
+namespace magic.data.cql.io
+{
+    /// <summary>
+    /// File service for Magic storing files in ScyllaDB.
+    /// </summary>
+    public class CqlStreamService : IStreamService
+    {
+        readonly IFileService _fileService;
+
+        /// <summary>
+        /// Creates an instance of your type.
+        /// </summary>
+        /// <param name="fileService">Underlaying file service.</param>
+        public CqlStreamService(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        /// <inheritdoc />
+        public Stream OpenFile(string path)
+        {
+            return OpenFileAsync(path).GetAwaiter().GetResult();
+        }
+
+        /// <inheritdoc />
+        public async Task<Stream> OpenFileAsync(string path)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(await _fileService.LoadAsync(path)));
+        }
+
+        /// <inheritdoc />
+        public void SaveFile(Stream stream, string path, bool overwrite)
+        {
+            SaveFileAsync(stream, path, overwrite).GetAwaiter().GetResult();
+        }
+
+        /// <inheritdoc />
+        public async Task SaveFileAsync(Stream stream, string path, bool overwrite)
+        {
+            var reader = new StreamReader(stream);
+            var content = await reader.ReadToEndAsync();
+            if (await _fileService.ExistsAsync(path))
+                await _fileService.DeleteAsync(path);
+            await _fileService.SaveAsync(path, content);
+        }
+    }
+}
