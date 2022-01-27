e39d30d56b16deeda940d9a815d5735bafd4133b
Thomas Hansen <thomas@gaiasoul.com>
2022-01-27 18:23:38 +0200

    More or less done, needs some polishing

--- magic.data.cql/caching/Caching.cs
+++ magic.data.cql/caching/Caching.cs
@@ -6,6 +6,7 @@ using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
+using Cassandra;
 using magic.node.contracts;
 using magic.node.extensions;
 using magic.data.cql.helpers;
@@ -37,7 +38,7 @@ namespace magic.data.cql.caching
         }
 
         /// <inheritdoc/>
-        public async Task<object> GetAsync(string key, bool hidden = false)
+        public Task<object> GetAsync(string key, bool hidden = false)
         {
             // Creating a cluster session using magic_cache keyspace.
             using (var session = Utilities.CreateSession(_configuration, "magic_cache"))
@@ -45,27 +46,37 @@ namespace magic.data.cql.caching
                 // Figuring out tenant and cloudlet values.
                 var ids = Utilities.Resolve(_rootResolver);
 
-                // Creating our CQL.
-                var cql = "select key, value from cache where tenant = ? and cloudlet = ? and key = ?";
-
-                // Executing CQL towards session.
-                var record = await Utilities.SingleAsync(
-                    session,
-                    cql, new object[] {
-                        ids.Tenant,
-                        ids.Cloudlet,
-                        ((hidden ? "." : "+") + key),
-                    });
-
-                // Returning match to caller if any.
-                return record?.GetValue<string>("value");
+                // Invoking helper method to return cache item.
+                return GetAsync(session, ids.Tenant, ids.Cloudlet, hidden, key);
             }
         }
 
         /// <inheritdoc/>
         public async Task<object> GetOrCreateAsync(string key, Func<Task<(object, DateTime)>> factory, bool hidden = false)
         {
-            throw new NotImplementedException();
+            // Creating a cluster session using magic_cache keyspace.
+            using (var session = Utilities.CreateSession(_configuration, "magic_cache"))
+            {
+                // Figuring out tenant and cloudlet values.
+                var ids = Utilities.Resolve(_rootResolver);
+
+                // Invoking helper method to return cache item.
+                var cached = await GetAsync(session, ids.Tenant, ids.Cloudlet, hidden, key);
+                if (cached != null)
+                    return cached;
+
+                // Invoking factory method.
+                var created = await factory();
+
+                // Figuring out TTL value of newly created item.
+                var ttl = created.Item2 > DateTime.UtcNow.AddYears(1) ? -1 : Convert.ToInt32((created.Item2 - DateTime.UtcNow).TotalSeconds);
+
+                // Upserting item.
+                await UpsertAsync(session, ids.Tenant, ids.Cloudlet, key, hidden, ttl, created.Item1);
+
+                // Returning item to caller.
+                return created.Item1;
+            }
         }
 
         /// <inheritdoc/>
@@ -127,16 +138,14 @@ namespace magic.data.cql.caching
         }
 
         /// <inheritdoc/>
-        public async Task UpsertAsync(string key, object value, DateTime utcExpiration, bool hidden = false)
+        public Task UpsertAsync(string key, object value, DateTime utcExpiration, bool hidden = false)
         {
             // Sanity checking invocation.
             if (utcExpiration < DateTime.UtcNow)
                 throw new HyperlambdaException($"You cannot upsert a new item into your cache with an expiration date that is in the past. Cache key of item that created conflict was '{key}'");
 
             // Calculating TTL in aseconds.
-            var ttl = Convert.ToUInt64((utcExpiration - DateTime.UtcNow).TotalSeconds);
-            if (ttl < 1L)
-                throw new HyperlambdaException("The minimum expiration time for cache items as 1 second into the future.");
+            var ttl = utcExpiration > DateTime.UtcNow.AddYears(1) ? -1 : Convert.ToInt32((utcExpiration - DateTime.UtcNow).TotalSeconds);
 
             // Creating a cluster session using magic_cache keyspace.
             using (var session = Utilities.CreateSession(_configuration, "magic_cache"))
@@ -144,19 +153,8 @@ namespace magic.data.cql.caching
                 // Figuring out tenant and cloudlet values.
                 var ids = Utilities.Resolve(_rootResolver);
 
-                // Creating our CQL.
-                var cql = $"insert into cache (tenant, cloudlet, key, value) values (?, ?, ?, ?) using ttl {ttl}";
-
-                // Executing CQL towards session.
-                await Utilities.ExecuteAsync(
-                    session,
-                    cql,
-                    new object[] {
-                        ids.Tenant,
-                        ids.Cloudlet,
-                        GetKey(key, hidden),
-                        value.ToString(),
-                    });
+                // Invoking helper method to upsert item.
+                return UpsertAsync(session, ids.Tenant, ids.Cloudlet, key, hidden, ttl, value);
             }
         }
 
@@ -172,6 +170,61 @@ namespace magic.data.cql.caching
 
             // Returning full key value to caller according to visibility.
             return (hidden ? "." : "+") + key;
+        }
+
+        /*
+         * Returning cache item from the specified session with the given values.
+         */
+        static async Task<object> GetAsync(
+            ISession session,
+            string tenant,
+            string cloudlet,
+            bool hidden,
+            string key)
+        {
+            // Creating our CQL.
+            var cql = "select key, value from cache where tenant = ? and cloudlet = ? and key = ?";
+
+            // Executing CQL towards session.
+            var record = await Utilities.SingleAsync(
+                session,
+                cql, new object[] {
+                    tenant,
+                    cloudlet,
+                    ((hidden ? "." : "+") + key),
+                });
+
+            // Returning match to caller if any.
+            return record?.GetValue<string>("value");
+        }
+
+        /*
+         * Helper method to upsert item.
+         */
+        static async Task UpsertAsync(
+            ISession session,
+            string tenant,
+            string cloudlet,
+            string key,
+            bool hidden,
+            int ttl,
+            object value)
+        {
+            // Creating our CQL.
+            var cql = ttl == -1 ? 
+                "insert into cache (tenant, cloudlet, key, value) values (?, ?, ?, ?)" :
+                $"insert into cache (tenant, cloudlet, key, value) values (?, ?, ?, ?) using ttl {ttl}";
+
+            // Executing CQL towards session.
+            await Utilities.ExecuteAsync(
+                session,
+                cql,
+                new object[] {
+                    tenant,
+                    cloudlet,
+                    GetKey(key, hidden),
+                    value.ToString(),
+                });
         }
 
         #endregion
